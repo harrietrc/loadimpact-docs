1b35f89772f3c5e721806d9d505410b078a8c9b2
Robin Gustafsson <robin@loadimpact.com>
2018-07-04 20:44:17 +0200

    [tools] Fix CF cache invalidation to only run if there are changes in the docs files, try 2

--- deploy.sh
+++ deploy.sh
@@ -18,7 +18,7 @@ aws s3 sync jekyll/_site/docs $s3_bucket --size-only --exclude "*" --include "*.
 aws s3 sync jekyll/_site/docs $s3_bucket --size-only --content-type text/html --exclude "*.*" --delete | tee -a $tempfile
 
 # Only invalidate CloudFront cache if we actually changed any files in the docs
-if [ ! -s $tempfile ]
+if [ -s $tempfile ]
 then
     echo "Invalidating cached files on CloudFront..."
     grep "upload\|deleted" $tempfile | sed -e "s|.*upload.*to $s3_bucket|/|" | sed -e "s|.*delete: $s3_bucket|/|" | sed -e 's/index.html//' | sed -e 's/\(.*\).html/\1/' | tr '\n' ' ' | xargs aws cloudfront create-invalidation --distribution-id $distribution_id --paths
