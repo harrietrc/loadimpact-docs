0549f42f69680c05fd25296802801b90192d9afa
Robin Gustafsson <robin@loadimpact.com>
2018-07-04 17:53:30 +0200

    [tools] Fix variable substitution

--- deploy.sh
+++ deploy.sh
@@ -14,8 +14,8 @@ set -v # echo on
 tempfile=$(mktemp)
 
 echo "Copying files to S3..."
-aws s3 sync jekyll/_site/docs $(s3_bucket) --size-only --exclude "*" --include "*.*" --delete | tee -a $(tempfile)
-aws s3 sync jekyll/_site/docs $(s3_bucket) --size-only --content-type text/html --exclude "*.*" --delete | tee -a $(tempfile)
+aws s3 sync jekyll/_site/docs $s3_bucket --size-only --exclude "*" --include "*.*" --delete | tee -a $tempfile
+aws s3 sync jekyll/_site/docs $s3_bucket --size-only --content-type text/html --exclude "*.*" --delete | tee -a $tempfile
 
 echo "Invalidating cached files on CloudFront..."
-grep "upload\|deleted" $(tempfile) | sed -e "s|.*upload.*to $(s3_bucket)|/|" | sed -e "s|.*delete: $(s3_bucket)|/|" | sed -e 's/index.html//' | sed -e 's/\(.*\).html/\1/' | tr '\n' ' ' | xargs aws cloudfront create-invalidation --distribution-id $(distribution_id) --paths
+grep "upload\|deleted" $tempfile | sed -e "s|.*upload.*to $s3_bucket|/|" | sed -e "s|.*delete: $s3_bucket|/|" | sed -e 's/index.html//' | sed -e 's/\(.*\).html/\1/' | tr '\n' ' ' | xargs aws cloudfront create-invalidation --distribution-id $distribution_id --paths
